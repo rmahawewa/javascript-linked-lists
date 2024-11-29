274bd09cf340a614b4e1e40f44d2535aee1c50b5
rmahawewa <rmahawewa@yahoo.com>
2024-11-29 21:21:31 +0530

    implement linkedlist

--- script1.js
+++ script1.js
@@ -1,48 +1,62 @@
-function linkedList(){
-    let list = node();
+let l1 = linkedList().list;
+console.log(l1);
+l1 = linkedList(l1).append("abcdefg");
+l1 = linkedList(l1).append("hijklmn");
+l1 = linkedList(l1).append("hijklmnopqrstuvw");
+// l1 = linkedList(l1).prepend("prepend value");
+console.log(l1);
+
+// console.log(linkedList().list);
+// console.log(linkedList().append("abc123"));
+// console.log(linkedList().append("abcdefg1234567"));
+
+function linkedList(list = null){
+    if(list == null){
+        list = node();
+    }    
+    
     let append = (value) => {
         let list_value = list.value;
         let next = list.nextNode;
 
-        if(next == null){
-            next = node();
-            next.set_value(value);
+        if(list_value == null && next == null){
+            list.value = value;
         }
 
-        while(next !== null){
-            let temp = next;
-            next = temp.nextNode;
+        if(list_value !== null && next == null){
+            next = node();
+            next.value = value;
+            list.nextNode = next;
         }
-        next = node();
-        next.set_value(value);
+
+        if(list_value !== null && next !== null){
+            while(next !== null){
+                let temp = next;
+                next = temp.nextNode;
+            }
+            next = node();
+            next.value = value;
+        }        
 
         return list;
     }
 
     let prepend = (value) => {
         let new_node = node();
-        new_node.set_value(value);
-        new_node.set_next(list);
+        new_node.value = value;
+        new_node.nextNode = list;
         return new_node;
     }
 
-    return {list, append};
+    return {list, append, prepend};
 }
 
 function node(){
     let value = null;
     let nextNode = null;
 
-    let set_value = (v) => {
-        value = v;
-        return value;
-    }
-
-    let set_next = (n) => {
-        nextNode = n;
-        return nextNode;
-    }
-
-    return {value, nextNode, set_value, set_next};
+    return {value, nextNode};
 }
+
+
 
