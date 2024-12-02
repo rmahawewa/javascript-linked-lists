bf98464e30e6f8d4d1671ded304cb7719d83e9c8
rmahawewa <rmahawewa@yahoo.com>
2024-12-02 21:17:33 +0530

    prepend

--- script1.js
+++ script1.js
@@ -2,8 +2,10 @@ let l1 = linkedList().list;
 console.log(l1);
 l1 = linkedList(l1).append("abcdefg");
 l1 = linkedList(l1).append("hijklmn");
-l1 = linkedList(l1).append("hijklmnopqrstuvw");
+l1 = linkedList(l1).append("jjjfgjrf");
+l1 = linkedList(l1).append("jjjfgjrfirijgdeug");
 l1 = linkedList(l1).prepend("prepend value");
+l1 = linkedList(l1).prepend("prepend value1");
 console.log(l1);
 
 // console.log(linkedList().list);
@@ -11,34 +13,78 @@ console.log(l1);
 // console.log(linkedList().append("abcdefg1234567"));
 
 function linkedList(list = null){
-    if(list == null){
-        list = node();
-    }    
+    let head = null;
+    let tail = null;
     
-    let append = (value) => {
-        let list_value = list.value;
-        let next = list.nextNode;
+    if(list === null){
+        list = node();
+        head = list;
+        tail = list;
+    }else{
+        head = list.head;
+        tail = list.tail;
+    }
+    
+    let append = (v) => {
 
-        if(list_value == null && next == null){
-            list.value = value;
+        if(head == tail == list){
+            console.log(head);
+            // return;
+            head.value = v;
+            tail = node();
+            head.nextNode = tail;
+            list.head = head;
+            list.tail = tail;
+
+            return list;
+        }
+        if(tail.value == null && tail.nextNode == null){
+            tail.value = v;
+            tail.nextNode = node();
+            list.tail = tail.nextNode;
+
+            return list;
+        }
+        if(tail.value !== null && tail.nextNode == null){
+            tail.nextNode = node();
+            tail.nextNode.value = v;
+            list.tail = tail.nextNode;
+
+            return list;
         }
 
-        if(list_value !== null && next == null){
-            next = node();
-            next.value = value;
-            list.nextNode = next;
-        }
 
-        if(list_value !== null && next !== null){
-            while(next !== null){
-                let temp = next;
-                next = temp.nextNode;
-            }
-            next = node();
-            next.value = value;
-        }        
 
-        return list;
+        // console.log("test12345");
+
+        // if(list.value == null && list.nextNode == null){
+        //     list.value = v;
+        //     return list;
+        // }
+
+        // if(list.value !== null && list.nextNode == null){
+        //     // debugger;
+        //     list.nextNode = node();
+        //     list.nextNode.value = v;
+        //     list.nextNode.nextNode = node();
+        //     return list;
+        // }
+
+        // if(list.value !== null && list.nextNode !== null){
+        //     let next = list.nextNode;
+        //     while(next !== null){
+        //         console.log(next.value);
+        //         next = next.nextNode;
+        //         console.log(next);
+        //     }
+        //     console.log(next);
+        //     // next = node();
+        //     next.value = v;
+        //     console.log(next);
+        //     return list;
+        // }        
+
+        return;
     }
 
     let prepend = (value) => {
